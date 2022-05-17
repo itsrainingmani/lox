45cacddaf2cbf56e25424dcbaf0c960ec0b6ef52
Manikandan Sundararajan <mani@hey.com>
2022-05-17 19:37:27 -0400

    adding new methods for variable parsing

--- java/com/itsrainingmani/lox/Parser.java
+++ java/com/itsrainingmani/lox/Parser.java
@@ -37,7 +37,7 @@ class Parser {
   List<Stmt> parse() {
     List<Stmt> statements = new ArrayList<>();
     while (!isAtEnd()) {
-      statements.add(statement());
+      statements.add(declaration());
     }
 
     return statements;
@@ -57,6 +57,18 @@ class Parser {
     return equality();
   }
 
+  private Stmt declaration() {
+    try {
+      if (match(VAR))
+        return varDeclaration();
+
+      return statement();
+    } catch (ParseError error) {
+      synchronize();
+      return null;
+    }
+  }
+
   private Stmt statement() {
     if (match(PRINT))
       return printStatement();
@@ -68,6 +80,18 @@ class Parser {
     Expr value = expression();
     consume(SEMICOLON, "Expect ';' after value.");
     return new Stmt.Print(value);
+  }
+
+  private Stmt varDeclaration() {
+    Token name = consume(IDENTIFIER, "Expect variable name.");
+
+    Expr initializer = null;
+    if (match(EQUAL)) {
+      initializer = expression();
+    }
+
+    consume(SEMICOLON, "Expect ';' after variable declaration");
+    return new Stmt.Var(name, initializer);
   }
 
   // Lets you place an expression where a statement is expected
@@ -149,6 +173,10 @@ class Parser {
 
     if (match(NUMBER, STRING)) {
       return new Expr.Literal(previous().literal);
+    }
+
+    if (match(IDENTIFIER)) {
+      return new Expr.Variable(previous());
     }
 
     if (match(LEFT_PAREN)) {
