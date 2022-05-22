e20832e39a3d70256fbb4b3ee42e1af4a4f75542
Manikandan Sundararajan <mani@hey.com>
2022-05-22 02:31:50 -0400

    parse a function declaration

--- java/com/itsrainingmani/tool/GenerateAst.java
+++ java/com/itsrainingmani/tool/GenerateAst.java
@@ -27,6 +27,8 @@ public class GenerateAst {
         "Block  : List<Stmt> statements",
         "Break      : ",
         "Expression : Expr expression",
+        "Function   : Token name, List<Token> params," +
+            " List<Stmt> body",
         "If         : Expr condition, Stmt thenBranch," +
             " Stmt elseBranch",
         "Print      : Expr expression",
