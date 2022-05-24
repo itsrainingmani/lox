7edbe1eae75dfaa00ac4a993d732bbb661569a36
Manikandan Sundararajan <mani@hey.com>
2022-05-24 01:34:46 -0400

    maintain a separate map of local resolutions

--- java/com/itsrainingmani/lox/Interpreter.java
+++ java/com/itsrainingmani/lox/Interpreter.java
@@ -1,12 +1,15 @@
 package com.itsrainingmani.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
   final Environment globals = new Environment(); // holds fixed reference to the outermost global environment
   private Environment environment = globals; // changes as we enter and exit local scopes. tracks current env
+  private final Map<Expr, Integer> locals = new HashMap<>();
 
   Interpreter() {
     globals.define("clock", new LoxCallable() {
@@ -56,6 +59,10 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
       Lox.runtimeError(error);
       return null;
     }
+  }
+
+  void resolve(Expr expr, int depth) {
+    locals.put(expr, depth);
   }
 
   @Override
@@ -328,13 +335,30 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
   @Override
   public Object visitVariableExpr(Expr.Variable expr) {
-    return environment.get(expr.name);
+    return lookUpVariable(expr.name, expr);
+  }
+
+  private Object lookUpVariable(Token name, Expr expr) {
+    Integer distance = locals.get(expr);
+    if (distance != null) {
+      return environment.getAt(distance, name.lexeme);
+    } else {
+      return globals.get(name);
+    }
   }
 
   @Override
   public Object visitAssignExpr(Expr.Assign expr) {
     Object value = evaluate(expr.value);
-    environment.assign(expr.name, value);
+    // environment.assign(expr.name, value);
+
+    Integer distance = locals.get(expr);
+    if (distance != null) {
+      environment.assignAt(distance, expr.name, value);
+    } else {
+      globals.assign(expr.name, value);
+    }
+
     return value;
   }
 
