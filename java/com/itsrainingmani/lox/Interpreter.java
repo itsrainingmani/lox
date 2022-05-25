49dca168327135a23d2a521af16d49d2df0a0c63
Manikandan Sundararajan <mani@hey.com>
2022-05-25 02:22:10 -0400

    interpret and implement the get expr in LoxInstance

--- java/com/itsrainingmani/lox/Interpreter.java
+++ java/com/itsrainingmani/lox/Interpreter.java
@@ -186,6 +186,16 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     return function.call(this, arguments);
   }
 
+  @Override
+  public Object visitGetExpr(Expr.Get expr) {
+    Object object = evaluate(expr.object);
+    if (object instanceof LoxInstance) {
+      return ((LoxInstance) object).get(expr.name);
+    }
+
+    throw new RuntimeError(expr.name, "Only instances have properties.");
+  }
+
   private void checkNumberOperands(Token operator, Object left, Object right) {
     if (left instanceof Double && right instanceof Double)
       return;
