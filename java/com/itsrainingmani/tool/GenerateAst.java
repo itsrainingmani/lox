9ef21f85082a116f7a98954380b8cab6f751ca4a
Manikandan Sundararajan <mani@hey.com>
2022-05-26 00:47:09 -0400

    add a superclass as an Expr.Variable

--- java/com/itsrainingmani/tool/GenerateAst.java
+++ java/com/itsrainingmani/tool/GenerateAst.java
@@ -29,7 +29,8 @@ public class GenerateAst {
 
     defineAst(outputDir, "Stmt", Arrays.asList(
         "Block  : List<Stmt> statements",
-        "Class      : Token name, List<Stmt.Function> methods",
+        "Class      : Token name, Expr.Variable superclass," +
+            " List<Stmt.Function> methods",
         "Break      : ",
         "Expression : Expr expression",
         "Function   : Token name, Expr.Function function",
