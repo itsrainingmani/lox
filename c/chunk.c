e61ba7d4e181c33e24b3658e44467c3273a5b906
Manikandan Sundararajan <mani@hey.com>
2022-05-27 10:08:33 -0500

    work with value array within chunk

--- c/chunk.c
+++ c/chunk.c
@@ -8,10 +8,12 @@ void initChunk(Chunk* chunk)
   chunk->count = 0;
   chunk->capacity = 0;
   chunk->code = NULL;
+  initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk* chunk) {
   FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  freeValueArray(&chunk->constants);
   initChunk(chunk);
 }
 
@@ -26,4 +28,10 @@ void writeChunk(Chunk* chunk, uint8_t byte)
 
   chunk->code[chunk->count] = byte;
   chunk->count++;
+}
+
+// Add a constant and return the index where the constant was appended so we can locate the same constant later
+int addConstant(Chunk* chunk, Value value) {
+  writeValueArray(&chunk->constants, value);
+  return chunk->constants.count - 1;
 }
--- c/chunk.h
+++ c/chunk.h
@@ -2,6 +2,7 @@
 #define clox_chunk_h
 
 #include "common.h"
+#include "value.h"
 
 typedef enum
 {
@@ -18,10 +19,14 @@ typedef struct
   int count;
   int capacity;
   uint8_t* code;
+  ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte);
+
+// convenience method to add a new constant to the chunk
+int addConstant(Chunk* chunk, Value value);
 
 #endif
