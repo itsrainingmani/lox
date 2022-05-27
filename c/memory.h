b44bf1df605228bd7227f7a4bb2fe145a1890df7
Manikandan Sundararajan <mani@hey.com>
2022-05-27 01:06:04 -0500

    minor

--- c/memory.h
+++ c/memory.h
@@ -7,7 +7,7 @@
   ((capacity) < 8 ? 8 : (capacity)*2)
 
 #define GROW_ARRAY(type, pointer, oldCount, newCount)    \
-  (type *)reallocate(pointer, sizeof(type) * (oldCount), \
+  (type*)reallocate(pointer, sizeof(type) * (oldCount), \
                      sizeof(type) * (newCount))
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
