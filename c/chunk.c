59c387c2af07f7f7f1e9e77d0e37641304073acb
Manikandan Sundararajan <mani@hey.com>
2022-05-27 01:40:27 -0500

    rearrange fn order. fix error in writeChunk

--- c/chunk.c
+++ c/chunk.c
@@ -10,6 +10,11 @@ void initChunk(Chunk* chunk)
   chunk->code = NULL;
 }
 
+void freeChunk(Chunk* chunk) {
+  FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  initChunk(chunk);
+}
+
 void writeChunk(Chunk* chunk, uint8_t byte)
 {
   if (chunk->capacity < chunk->count + 1)
@@ -17,13 +22,8 @@ void writeChunk(Chunk* chunk, uint8_t byte)
     int oldCapacity = chunk->capacity;
     chunk->capacity = GROW_CAPACITY(oldCapacity);
     chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
-
-    chunk->code[chunk->count] = byte;
-    chunk->code++;
   }
-}
 
-void freeChunk(Chunk* chunk) {
-  FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
-  initChunk(chunk);
+  chunk->code[chunk->count] = byte;
+  chunk->code++;
 }
