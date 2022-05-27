781f7dd28ff398a5ca59641bef1a58c298df8331
Manikandan Sundararajan <mani@hey.com>
2022-05-27 00:49:34 -0500

    module that defines our bytecode repr

---
+++ c/chunk.h
@@ -0,0 +1,26 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+
+typedef enum
+{
+  OP_RETURN,
+} OpCode;
+
+/* Dynamic array - wrapper around an array of bytes
+  Cache-friendly, dense storage
+  Constant-time indexed element lookup
+  Constant-time appending to end of array
+*/
+typedef struct
+{
+  int count;
+  int capacity;
+  uint8_t *code;
+} Chunk;
+
+void initChunk(Chunk *chunk);
+void writeChunk(Chunk *chunk, uint8_t byte);
+
+#endif
