0083e92fec4e70520584840b407a05b1e72fd96a
Manikandan Sundararajan <mani@hey.com>
2022-05-27 01:11:43 -0500

    hand build a sample return bytecode

--- c/main.c
+++ c/main.c
@@ -1,6 +1,13 @@
 #include "common.h"
+#include "chunk.h"
 
 int main(int argc, const char* argv[])
 {
+  Chunk chunk;
+  initChunk(&chunk);
+  writeChunk(&chunk, OP_RETURN);
+  freeChunk(&chunk);
+
+  // disassembleChunk(&chunk, "test chunk");
   return 0;
 }
