f832fdc9846b993ae61950af4ab83380f7e86e29
Manikandan Sundararajan <mani@hey.com>
2022-05-27 22:25:47 -0500

    handle OP_CONSTANT in the VM (temp)

--- c/vm.c
+++ c/vm.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "common.h"
 #include "vm.h"
 
@@ -13,15 +15,26 @@ void freeVM() {
 
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
   for (;;) {
     uint8_t instruction;
+
+    // READ_BYTE reads the byte currently pointed at by ip and then advances the instruction pointer. The first byte of any instruction is the opcode.
+    // Given a numeric opcode, we need to get to the right C code that implements the semantics. this is called decoding or dispatching the instructions
     switch (instruction = READ_BYTE()) {
+    case OP_CONSTANT: {
+      Value constant = READ_CONSTANT();
+      printValue(constant);
+      printf("\n");
+      break;
+    }
     case OP_RETURN: {
       return INTERPRET_OK;
     }
     }
   }
 #undef READ_BYTE
+#undef READ_CONSTANT
 }
 
 InterpretResult interpret(Chunk* chunk) {
