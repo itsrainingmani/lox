63bdcf7ae94bd4e6074ed38924a0d7cf6b425d0c
Manikandan Sundararajan <mani@hey.com>
2022-06-01 20:31:59 -0400

    binary arithmetic

--- c/vm.c
+++ c/vm.c
@@ -32,6 +32,13 @@ Value pop() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+  do { \
+    double b = pop(); \
+    double a = pop(); \
+    push(a op b); \
+  } while (false)
+
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
     printf("          ");
@@ -50,12 +57,17 @@ static InterpretResult run() {
     // Given a numeric opcode, we need to get to the right C code that
     // implements the semantics. this is called decoding or dispatching the
     // instructions
-    switch (instruction = READ_BYTE()) {
+    switch (instruction = READ_BYTE())
+    {
     case OP_CONSTANT: {
       Value constant = READ_CONSTANT();
       push(constant);
       break;
     }
+    case OP_ADD: BINARY_OP(+); break;
+    case OP_SUBTRACT: BINARY_OP(-); break;
+    case OP_MULTIPLY: BINARY_OP(*); break;
+    case OP_DIVIDE: BINARY_OP(/ ); break;
     case OP_NEGATE: push(-pop()); break;
     case OP_RETURN: {
       printValue(pop());
@@ -66,6 +78,7 @@ static InterpretResult run() {
   }
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk) {
