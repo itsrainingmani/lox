e4947d7fb34da7d9a27c5826c87a4d1daf3d725c
Manikandan Sundararajan <mani@hey.com>
2022-06-05 01:11:03 -0400

    add some token constructor methods

--- c/scanner.c
+++ c/scanner.c
@@ -12,8 +12,38 @@ typedef struct {
 
 Scanner scanner;
 
+static bool isAtEnd() {
+  return *scanner.current == '\0';
+}
+
+static Token makeToken(TokenType type) {
+  Token token;
+  token.type = type;
+  token.start = scanner.start;
+  token.length = (int)(scanner.current - scanner.start);
+  token.line = scanner.line;
+  return token;
+}
+
+static Token errorToken(const char* message) {
+  Token token;
+  token.type = TOKEN_ERROR;
+  token.start = message;
+  token.length = (int)strlen(message);
+  token.line = scanner.line;
+  return token;
+}
+
 void initScanner(const char* source) {
   scanner.start = source;
   scanner.current = source;
   scanner.line = 1;
+}
+
+Token scanToken() {
+  scanner.start = scanner.current;
+
+  if (isAtEnd()) return makeToken(TOKEN_EOF);
+
+  return errorToken("Unexpected character.");
 }
