86dfe26ba30f1ed9a70976c42def7cfc9fc1686f
Manikandan Sundararajan <mani@hey.com>
2023-02-12 03:01:16 -0500

    Simple tagged union for Values and some macros

--- c/value.h
+++ c/value.h
@@ -3,6 +3,38 @@
 
 #include "common.h"
 
+typedef enum {
+  VAL_BOOL,
+  VAL_NIL,
+  VAL_NUMBER,
+} ValueType;
+
+typedef struct {
+  ValueType type;
+  union {
+    bool boolean;
+    double number;
+  } as;
+} Value;
+
+// A Value (struct) can contain a double but is not equivalent to it
+// There is a mandatory conversion step from one to the other
+
+// Macros to check a Value's type
+#define IS_BOOL(value)    ((value).type == VAL_BOOL)
+#define IS_NIL(value)     ((value).type == VAL_NIL)
+#define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
+
+// Given a Value of the right type, these macros unwrap it and
+// return the corresponding raw C value
+#define AS_BOOL(value)   ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+
+// Promoting a native C value to a clox Value
+#define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+
 typedef double Value;
 
 typedef struct {
