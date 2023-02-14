43aa8bdb90660afcbc2241c02a2b59372c8797b6
Manikandan Sundararajan <mani@hey.com>
2023-02-13 19:45:33 -0500

    Move initScanner to the right position in scanner

--- c/scanner.c
+++ c/scanner.c
@@ -12,6 +12,21 @@ typedef struct {
 
 Scanner scanner;
 
+void initScanner(const char* source) {
+  scanner.start = source;
+  scanner.current = source;
+  scanner.line = 1;
+}
+
+static bool isAlpha(char c) {
+  return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+}
+
+
+static bool isDigit(char c) {
+  return c >= '0' && c <= '9';
+}
+
 static bool isAtEnd() {
   return *scanner.current == '\0';
 }
@@ -171,20 +186,6 @@ static Token string() {
   // the closing quote
   advance();
   return makeToken(TOKEN_STRING);
-}
-
-void initScanner(const char* source) {
-  scanner.start = source;
-  scanner.current = source;
-  scanner.line = 1;
-}
-
-static bool isAlpha(char c) {
-  return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
-}
-
-static bool isDigit(char c) {
-  return c >= '0' && c <= '9';
 }
 
 Token scanToken() {
