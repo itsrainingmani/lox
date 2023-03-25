10c0271c3e2b00dd42c5769b34d21da371b513ba
Manikandan Sundararajan <mani@hey.com>
2023-03-25 02:50:58 -0400

    Initialize a hash table

---
+++ c/table.h
@@ -0,0 +1,27 @@
+#ifndef clox_table_h
+#define clox_table_h
+
+#include "common.h"
+#include "value.h"
+
+/// @brief Entry is a kv pair. Key is always a string so we store the ObjString pointer directly (faster and smaller this way)
+typedef struct
+{
+  ObjString* key;
+  Value value;
+} Entry;
+
+
+/// @brief Hash Table is an array of entries. We keep track of both allocated size of array (capacity) and the number of kv pairs currently stored (count).
+/// Ratio of count to capacity is the load factor of the hash table
+typedef struct
+{
+  int count;
+  int capacity;
+  Entry* entries;
+} Table;
+
+void initTable(Table* table);
+void freeTable(Table* table);
+
+#endif
