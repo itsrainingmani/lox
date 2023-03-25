937cc6824889bbb18ea9fb4ee9bd1c7edaa40b0b
Manikandan Sundararajan <mani@hey.com>
2023-03-25 02:56:48 -0400

    implement and use hash function when initializing, copying strings

--- c/object.c
+++ c/object.c
@@ -25,25 +25,41 @@ static Obj* allocateObject(size_t size, ObjType type) {
 /// @param chars 
 /// @param length 
 /// @return 
-static ObjString* allocateString(char* chars, int length) {
+static ObjString* allocateString(char* chars, int length, uint32_t hash) {
   ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
   string->length = length;
   string->chars = chars;
+  string->hash = hash;
   return string;
 }
 
+/// @brief Implementation of FNV-1a Hashing Algorithm
+/// @param key 
+/// @param length 
+/// @return Hashed value
+static uint32_t hashString(const chars* key, int length) {
+  uint32_t hash = 2166136261u;
+  for (int i = 0; i < length; i++) {
+    hash ^= (uint8_t)key[i];
+    hash *= 16777619;
+  }
+  return hash;
+}
+
+/// @brief takeString() claims ownership of the string you give it
 ObjString* takeString(char* chars, int length) {
-  // This function claims ownership of the string you give it
-  return allocateString(chars, length);
+  uint32_t hash = hashString(chars, length);
+  return allocateString(chars, length, hash);
 }
 
 ObjString* copyString(const char* chars, int length) {
+  uint32_t hash = hashString(chars, length);
   // Allocating array on the heap that is big enough for the string
   // and the trailing terminator
   char* heapChars = ALLOCATE(char, length + 1);
   memcpy(heapChars, chars, length);
   heapChars[length] = '\0';
-  return allocateString(heapChars, length);
+  return allocateString(heapChars, length, hash);
 }
 
 void printObject(Value value) {
